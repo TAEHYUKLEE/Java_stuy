f9e49bc514cf29efb7d97dc59cb5a93f9c75eeba
TAYLEE <89365465+TAEHYUKLEE@users.noreply.github.com>
2023-06-01 12:46:47 +0900

    생성자 에러 정리

--- Inheritance/ImplicitErrorParenet/Parenet.java
+++
@@ -1,9 +0,0 @@
-package ImplicitErrorParenet;
-
-public class Parenet {
-    int a, b;
-
-    public Parenet(int c){
-        this.a =c;
-    }
-}
---
+++ Inheritance/ImplicitErrorParenet/Parent.java
@@ -0,0 +1,16 @@
+package ImplicitErrorParenet;
+
+public class Parent {
+    int a, b;
+
+    //부모에게는 매개변수 생성자로 초기화하는 로직이 존재함.
+    public Parenet(int c){
+        this.a =c;
+    }
+
+    public Parent
+
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+}
